72d038a251947d3e6d9bacda2445778b7da38580
stephenkaraga <stephenkaraga@gmail.com>
2016-09-04 20:47:00 -0400

    Webpack setup

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const express = require('express');
 const app = new express();
 const cradle = require('cradle');
 const db = new(cradle.Connection)().database('initial');
+const bodyparser = require('body-parser');
+const errorHandler = require('errorhandler');
+const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
 
 app.get('/', function(request, response) {
 
